17acd440b82a33abd62401838622d99a257e7ab8
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-04-13 12:38:15 +0530

    add example tuple usage in TypeScript

---
+++ src/tuple.ts
@@ -0,0 +1,8 @@
+// let user: [string, number];
+// user = ["parvez", 8];
+
+// function tople(a: string, b: number): [string, number] {
+//   return [a, b];
+// }
+
+// console.log(tople("parvez", 343));
