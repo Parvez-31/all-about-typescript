5a4b87ceab90f41e9d0df4db0af2315030718e5b
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-04-14 21:28:12 +0530

    refactor: restructure script to include variable declarations and examples

--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,14 @@
-const arr: number[] = [1, 2, 3, 4, 5, 6];
-console.log(arr);
+// normal
+let name: string = "xyz";
+let age: number = 29;
+let isStudent: boolean = false;
+
+// array
+let numbers: number[] = [1, 2, 3, 4];
+let names: string[] = ["a", "b", "c"];
+
+// object
+let user: { name: string; age: number } = {
+  name: "Parvez",
+  age: 26,
+};
